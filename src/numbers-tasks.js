6ba7b5dba6ecd1f58ca5b1af9f0ab1e158812ec9
Aliseiko <aliseiko.dev@gmail.com>
2023-11-08 17:27:11 +0300

    feat: add 27-33 solutions

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -388,8 +388,9 @@ function toFixed(number, fractionDigits) {
  * 12345, 7    => '12345.00'
  * 12.345, 4   => '12.35'
  */
-function toPrecision(/* number, precision */) {
-  throw new Error('Not implemented');
+function toPrecision(number, precision) {
+  // const digitLength = number.toString().length;
+  return number.toPrecision(precision);
 }
 
 /**
@@ -402,8 +403,8 @@ function toPrecision(/* number, precision */) {
  * new Number(5) => 5
  * Number(-5)    => -5
  */
-function getNumberValue(/* number */) {
-  throw new Error('Not implemented');
+function getNumberValue(number) {
+  return number.valueOf();
 }
 
 /**
@@ -421,8 +422,9 @@ function getNumberValue(/* number */) {
  * 5        => true
  * '5'      => false
  */
-function isNumber(/* number */) {
-  throw new Error('Not implemented');
+function isNumber(number) {
+  // return typeof number === 'number' && Number.isFinite(number);
+  return !Number.isNaN(parseFloat(number)) && Number.isFinite(number);
 }
 
 /**
